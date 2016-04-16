e82f1fd8a8145ab12c346af099ae4681f6ca7fb1
Alexander Pronin <pronin.alx@gmail.com>
2016-04-16 12:16:56 +0300

    Final commit

--- 007_XMLChallengeStarter.playground/Contents.swift
+++ 007_XMLChallengeStarter.playground/Contents.swift
@@ -13,77 +13,98 @@ import UIKit
 //: The object is also a type of CustomStringConvertible. This simply means that the object can be printed to the console.
 
 class RWDevConSession: CustomStringConvertible {
-  var sessionId = 0
-  var name = ""
-  var instructor = ""
-  var track = ""
-  
-  var description: String {
-    return "Session \(sessionId): \(name) by \(instructor) - \(track) Track"
-  }
+    var sessionId = 0
+    var name = ""
+    var instructor = ""
+    var track = ""
+    
+    var description: String {
+        return "Session \(sessionId): \(name) by \(instructor) - \(track) Track"
+    }
 }
 
 //: Now is the time to create your XML parsing object. Call the object SessionParser and make it a subclass of NSObject. Also, make it implement the NSXMLParserDelegate protocol.
 
-//class  {
-  
-//: This object should have four properties. It should contain an NSXMLPaser. Call it xmlParser. It should contain an array of RWDevConSession objects. Another variable called currentSession should be an optional RWDevConSession object. Finally, it should have a string called xmlText.
-  
-
-  
-//: Next comes the initializer. It should take in an NSData which is the xml data.
-
-//: Here's the code of init()
-//    self.xmlParser = NSXMLParser(data: xml)
-  
- 
-//: Here's the code used to kick off the parsing. This has also been provided for you.
-
-//  func parse() -> [RWDevConSession] {
-//    xmlParser.delegate = self
-//    xmlParser.parse()
-//    return sessions
-//  }
-  
-//: Add the code to the following method body:
-
-//  func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
-
-//  }
-  
-//: Add the code to the following method body:
-
-//  func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-
-//  }
-  
-//: Add the code to the following method body:
-
-//  func parser(parser: NSXMLParser, foundCharacters string: String) {
-
-//  }
-  
-//}
+class SessionParser: NSObject, NSXMLParserDelegate  {
+    
+    //: This object should have four properties. It should contain an NSXMLPaser. Call it xmlParser. It should contain an array of RWDevConSession objects. Another variable called currentSession should be an optional RWDevConSession object. Finally, it should have a string called xmlText.
+    var xmlParser: NSXMLParser?
+    var sessions: [RWDevConSession] = []
+    var currentSession: RWDevConSession?
+    var xmlText = ""
+    //: Next comes the initializer. It should take in an NSData which is the xml data.
+    
+    //: Here's the code of init()
+    init(withXML xml: String) {
+        if let data = xml.dataUsingEncoding(NSUTF8StringEncoding) {
+            xmlParser = NSXMLParser(data: data)
+        }
+    }
+    
+    //: Here's the code used to kick off the parsing. This has also been provided for you.
+    
+    func parse() -> [RWDevConSession] {
+        xmlParser?.delegate = self
+        xmlParser?.parse()
+        return sessions
+    }
+    
+    //: Add the code to the following method body:
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        xmlText = ""
+        if elementName == "session" {
+            currentSession = RWDevConSession()
+        }
+    }
+    
+    //: Add the code to the following method body:
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "sessionId" {
+            if let sessionId = Int(xmlText) {
+                currentSession?.sessionId = sessionId
+            }
+        }
+        if elementName == "name" {
+            currentSession?.name = xmlText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        }
+        if elementName == "instructor" {
+            currentSession?.instructor = xmlText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        }
+        if elementName == "track" {
+            currentSession?.track = xmlText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        }
+        if elementName == "session" {
+            if let session = currentSession {
+                sessions.append(session)
+            }
+        }
+    }
+    //
+    //: Add the code to the following method body:
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        xmlText += string
+    }
+    
+}
 
 //: Once you have the parser written, the following code will work. It will read the xml and print out three different sessions to the console. If you find yourself stuck, check out the completed challenge playground.
 
-/**
 
 var sessions = [RWDevConSession]()
 
 if let xmlUrl = NSBundle.mainBundle().URLForResource("rwdevcon", withExtension: "xml") {
-  do {
-    let xmlText = try String(contentsOfURL: xmlUrl)
-    if let xmlData = xmlText.dataUsingEncoding(NSUTF8StringEncoding) {
-      let parser = SessionsParser(withXML: xmlData)
-      let sessions = parser.parse()
-      for session in sessions {
-        print(session)
-      }
+    do {
+        let xmlText = try String(contentsOfURL: xmlUrl)
+        let parser = SessionParser(withXML: xmlText)
+        let sessions = parser.parse()
+        for session in sessions {
+            print(session)
+        }
+    } catch {
+        print(error)
     }
-  } catch {
-    print(error)
-  }
 }
-
-**/
+ 
